db599dd3f9489885fb8302c57a57ca58c19284c7
Mitsutoshi Aoe <maoe@foldr.in>
2012-01-21 01:14:19 +0900

    Add command-line options: --exec, --port and --host

--- Main.hs
+++ Main.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE ScopedTypeVariables, QuasiQuotes, OverloadedStrings #-}
-{-# OPTIONS -Wall #-}
+{-# LANGUAGE ScopedTypeVariables, QuasiQuotes, OverloadedStrings, RecordWildCards, DeriveDataTypeable #-}
 module Main where
 import Control.Concurrent (Chan, newChan, writeChan, forkIO)
 import Control.Exception (IOException, try)
 import Control.Monad (void)
 import Control.Monad.Trans (MonadIO(..), MonadTrans(..))
+import Data.Foldable (forM_)
+import Data.Maybe (maybeToList)
 import Data.Text (Text)
-import Network (Socket, PortID(..), listenOn)
+import Network (PortID(..), listenOn, sClose)
 import System.IO (stdin, stdout)
+import System.Process (rawSystem)
 
 import Blaze.ByteString.Builder.Char.Utf8 (fromText)
 import Data.Conduit (($$), (=$), ($=), ResourceIO, Source, Sink, SinkResult(..), Conduit, runResourceT, sinkIO)
@@ -16,27 +18,39 @@ import Data.Conduit.Text (encode, decode, utf8)
 import Network.HTTP.Types (headerContentType, statusOK, statusNotFound)
 import Network.Wai (Application, Request(..), Response(..), responseLBS)
 import Network.Wai.EventSource (ServerEvent(..), eventSourceApp)
-import Network.Wai.Handler.Warp (runSettingsSocket, defaultSettings)
+import Network.Wai.Handler.Warp (runSettings, defaultSettings, settingsHost, settingsPort)
+import System.Console.CmdArgs
 import Text.Blaze.Renderer.Utf8 (renderHtmlBuilder)
 import Text.Hamlet (Html, shamlet)
 import qualified Data.Conduit.List as CL
 
 main :: IO ()
 main = do
+  HtmlCat {..} <- cmdArgs htmlCat
   chan <- newChan
-  sock <- newSock
-  runSettingsSocket defaultSettings sock (app chan)
+  port <- newPort _port
+  let url = "http://" ++ _host ++ ":" ++ show port
+  putStrLn url
+  whenJust _exec $ \exec ->
+    forkIO $ void $ rawSystem exec [url]
+  runSettings (defaultSettings { settingsHost = _host
+                               , settingsPort = port })
+              (app chan)
+  where
+    whenJust = forM_
 
-newSock :: IO Socket
-newSock = foldr tryListening (error "no available port") [45192..60000]
+newPort :: Maybe Int -> IO Int
+newPort port'm = foldr tryListening
+                       (error "no available port")
+                       (maybeToList port'm ++ [45192..60000])
   where
     tryListening p next = do
-      r <- try . listenOn $ PortNumber p
+      r <- try . listenOn $ PortNumber (fromIntegral p)
       case r of
         Left (_ :: IOException) -> next
         Right sock -> do
-          putStrLn $ "http://localhost:" ++ show p
-          return sock
+          sClose sock
+          return p
 
 app :: Chan ServerEvent -> Application
 app chan req =
@@ -80,6 +94,19 @@ sinkChan chan = sinkIO noop (const noop) push return
     push _ a = do
       liftIO $ writeChan chan a
       return Processing
+
+data HtmlCat = HtmlCat
+  { _port :: Maybe Int
+  , _host :: String
+  , _exec :: Maybe String
+  } deriving (Show, Data, Typeable)
+
+htmlCat :: HtmlCat
+htmlCat = HtmlCat
+  { _port = Nothing     &= explicit &= name "port"
+  , _host = "127.0.0.1" &= explicit &= name "host"
+  , _exec = Nothing     &= explicit &= name "exec"
+  }
 
 html :: Html
 html = [shamlet|
